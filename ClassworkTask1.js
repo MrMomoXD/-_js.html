e124761bb2165a8ac34986226b00854adf8198dd
MrMomoXD <113432260+MrMomoXD@users.noreply.github.com>
2022-09-26 18:27:50 +0300

    Update ClassworkTask1.js

--- ClassworkTask1.js
+++ ClassworkTask1.js
@@ -1,9 +1,21 @@
 <!DOCTYPE html>
 <html>
 <head>
+<script type="text/javascript">
+var imgObj = null;          
+function init() {
+imgObj = document.getElementById('myImage');
+imgObj.style.position= 'relative'; 
+imgObj.style.left = '0px'; 
+}
+function moveRight() {
+imgObj.style.left = parseInt(imgObj.style.left) + 10 + 'px';
+}         
+window.onload = init;
+</script>
 <style>
 .button {
-  background-color: #4CAF50; /* Green */
+  background-color: #4CAF50;
   border: none;
   color: white;
   padding: 16px 32px;
@@ -34,8 +46,10 @@
   <input type="text" id="email" name="email">
   <label for="lname">Subscribe:</label>
   <input type="text" id="subscribe" name="subscribe">
+  <button type="submit" form="frm1" value="Submit" class="button button1">Submit</button><br><br>
+  <img id="myImage" src="img_girl.jpg" alt="Girl in a jacket" style="position: relative; left: 10px; "width="100" height="100">
+  <p>Click button below to move the image to right</p>
+  <input type="button" value="Click Me" onclick="moveRight();">
 </form>
-<button type="submit" form="frm1" value="Submit" class="button button1">Confirm</button>
-
 </body>
 </html>
