c52fd483fc7739326c0ffc46c35aba89f1407ec2
MrMomoXD <113432260+MrMomoXD@users.noreply.github.com>
2022-09-26 17:31:53 +0300

    Update ClassworkTask1.js

--- ClassworkTask1.js
+++ ClassworkTask1.js
@@ -1,20 +1,39 @@
 <!DOCTYPE html>
 <html>
+<head>
+<style>
+.button {
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 16px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+}
+
+.button1 {
+  background-color: white; 
+  color: black; 
+  border: 2px solid #4CAF50;
+}
+.button1:hover {
+  background-color: #4CAF50;
+  color: white;
+}
+</style>
+</head>
 <body>
 
 <form id="frm1" action="/action_page.php">
   Email: <input type="text" name="email">
   Subscribe: <input type="text" name="subscribe"><br><br>
 </form>
-<button id="btn" type="button" class="button">Click me!</button>
-<script>
-const btn = document.getElementById('btn');
-
-btn.addEventListener('click', function onClick() {
-  btn.style.backgroundColor = 'salmon';
-  btn.style.color = 'white';
-});
-</script>
+<button class="button button1">Confirm</button>
 
 </body>
 </html>
