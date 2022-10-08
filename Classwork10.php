407bc94bec276d00beb4c8c3d0020896dfa56c8c
MrMomoXD <113432260+MrMomoXD@users.noreply.github.com>
2022-10-08 16:35:07 +0300

    Update Classwork10.php

--- Classwork10.php
+++ Classwork10.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<?php
-echo "Today is " . date("Y/m/d") . "<br>";
-echo "Today is " . date("Y.m.d") . "<br>";
-echo "Today is " . date("Y-m-d") . "<br>";
-echo "Today is " . date("l");
-?>
-
-</body>
-</html>
+©<?php echo "Today is " . date("Y/m/d") . "<br>";
+date_default_timezone_set("Africa/Addis_Ababa");?>
+©<?php echo "The time is " . date("h:i:sa");?><br>
+Name: <?php echo $_GET["name"]; ?><br>
+Surname: <?php echo $_GET["surname"]; ?><br>
+Date of birth: <?php echo $_GET["date"]; ?>
